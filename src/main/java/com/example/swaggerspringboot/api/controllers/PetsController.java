6ff7121885c05e62d232d664f5774086b7e6cb79
ryo-murai <murai.ry@gmail.com>
2019-10-18 17:59:32 +0900

    implement listPets

--- src/main/java/com/example/swaggerspringboot/api/controllers/PetsController.java
+++ src/main/java/com/example/swaggerspringboot/api/controllers/PetsController.java
@@ -1,10 +1,15 @@
 package com.example.swaggerspringboot.api.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.swaggerspringboot.api.models.Pet;
 import com.example.swaggerspringboot.api.models.Pets;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class PetsController implements PetsApi {
+	
 	@Override
 	public ResponseEntity<Pets> listPets(@Valid Integer limit) {
 		log.info("limit param value:{}", limit);
-		return PetsApi.super.listPets(limit);
+		List<Pet> pets = LongStream.range(0, 5)
+			.mapToObj(n -> new Pet().id(n).name("pet" + n))
+			.collect(Collectors.toList());
+
+		Pets response = new Pets();
+		response.addAll(pets);
+		return ResponseEntity.ok(response);
 	}
 }
