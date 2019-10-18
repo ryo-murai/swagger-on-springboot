9e8432f6106b4538a1cc05e3b9d429234462468e
ryo-murai <murai.ry@gmail.com>
2019-10-18 18:30:03 +0900

    add createPets

--- src/main/java/com/example/swaggerspringboot/api/controllers/PetsController.java
+++ src/main/java/com/example/swaggerspringboot/api/controllers/PetsController.java
@@ -1,11 +1,13 @@
 package com.example.swaggerspringboot.api.controllers;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +19,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class PetsController implements PetsApi {
+	private final List<Pet> pets;
+	
+	public PetsController() {
+		pets = Collections.synchronizedList(
+				LongStream.range(0, 5)
+				.mapToObj(n -> new Pet().id(n).name("pet" + n))
+				.collect(Collectors.toList()));
+	}
+	
+	@Override
+	public ResponseEntity<Void> createPets(@Valid Pet body) {
+		pets.add(body);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
 	
 	@Override
 	public ResponseEntity<Pets> listPets(@Valid Integer limit) {
 		log.info("limit param value:{}", limit);
-		List<Pet> pets = LongStream.range(0, 5)
-			.mapToObj(n -> new Pet().id(n).name("pet" + n))
-			.collect(Collectors.toList());
 
 		Pets response = new Pets();
 		response.addAll(pets);
 		return ResponseEntity.ok(response);
 	}
+	
 }
